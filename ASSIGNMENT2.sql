46c3684d2a369a0b3161d55c97c271ab528ccaf6
supriya rani dhal <134327134+supriyarani-dhal@users.noreply.github.com>
2025-04-09 15:44:50 +0530

    Add files via upload

---
+++ ASSIGNMENT2.sql
@@ -0,0 +1,46 @@
+USE [SupriyaTask1]
+
+create procedure AMS.Proc_UserAndAll_Insert
+ @UserName	nvarchar(250),
+ @DOB	datetime,
+ @DOJ	datetime,
+ @Balance	decimal(10, 6),
+ @AccountNo	int,
+ @MobileNo	int,
+ @AddressDetail nvarchar(max),
+ @IsSaving bit,
+ @Amount decimal(20,2),
+ @IsDebit bit,
+ @CreatedBy	varchar(250) = 'defaultuser'
+as begin
+    
+   declare @UserID bigint
+   declare @AccountID bigint
+
+   insert into AMS.[User] (UserName, DOB, DOJ, Balance, AccountNo, MobileNo, CreatedBy) values
+   (@UserName, @DOB, @DOJ, @Balance, @AccountNo, @MobileNo, @CreatedBy)
+
+   set @UserId = scope_identity()
+
+   insert into AMS.[Address](UserId, AddressDetail, CreatedBy) 
+   values (@UserId, @AddressDetail, @CreatedBy)
+
+   INSERT INTO AMS.[Account](AccountNo, IsSaving, CreatedBy)
+   VALUES(@AccountNo, @IsSaving, @CreatedBy)
+
+   set @AccountID = SCOPE_IDENTITY()
+
+   INSERT INTO AMS.UserAccountMapping(UserID, AccountID, CreatedBy)
+   VALUES(@UserID, @AccountID, @CreatedBy)
+   
+   INSERT INTO AMS.AccountTransaction(AccountID, Amount, IsDebit, CreatedBy)
+   VALUES(@AccountID, @Amount, @IsDebit, @CreatedBy)
+end
+
+exec AMS.Proc_UserAndAll_Insert 'Harihara', '2000-12-25', '2020-08-13', 1200.0, 12345678, 87654321, 'Himalaya', 1, 5000.0, 0
+
+SELECT * FROM AMS.[User];
+SELECT * FROM AMS.[Address];
+SELECT * FROM AMS.Account;
+SELECT * FROM AMS.UserAccountMapping;
+SELECT * FROM AMS.AccountTransaction;
