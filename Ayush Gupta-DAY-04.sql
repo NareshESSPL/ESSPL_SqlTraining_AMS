19d7c6e8de61de1ca52bc87f3d1476557630f8c8
Ayush gupta <151567407+Ayushdevgupta@users.noreply.github.com>
2025-04-14 22:18:18 +0530

    Add files via upload

---
+++ Ayush Gupta-DAY-04.sql
@@ -0,0 +1,45 @@
+IF OBJECT_ID('dbo.TestMultiInput') IS NOT NULL
+DROP PROC dbo.TestMultiInput;
+GO
+
+-- Create the stored procedure
+CREATE PROCEDURE TestMultiInput
+    @Input VARCHAR(MAX)
+AS
+BEGIN
+    DECLARE @Str VARCHAR(100), @AccountID VARCHAR(100), @AccountNo VARCHAR(100)
+
+    -- Table to store results
+    DECLARE @Result TABLE (
+        AccountID VARCHAR(100),
+        AccountNo VARCHAR(100)
+    );
+
+    -- Loop to split input by comma
+    WHILE CHARINDEX(',', @Input) > 0
+    BEGIN
+        SET @Str = LTRIM(RTRIM(LEFT(@Input, CHARINDEX(',', @Input) - 1)));
+
+        SET @AccountID = LEFT(@Str, CHARINDEX(':', @Str) - 1);
+        SET @AccountNo = RIGHT(@Str, LEN(@Str) - CHARINDEX(':', @Str));
+
+        INSERT INTO @Result VALUES (@AccountID, @AccountNo);
+
+        SET @Input = LTRIM(RTRIM(STUFF(@Input, 1, CHARINDEX(',', @Input), '')))
+    END
+
+    -- Handle the last value (if any)
+    IF LEN(@Input) > 0 AND CHARINDEX(':', @Input) > 0
+    BEGIN
+        SET @AccountID = LEFT(@Input, CHARINDEX(':', @Input) - 1);
+        SET @AccountNo = RIGHT(@Input, LEN(@Input) - CHARINDEX(':', @Input));
+
+        INSERT INTO @Result VALUES (@AccountID, @AccountNo);
+    END
+
+    -- Final output
+    SELECT AccountID, AccountNo FROM @Result;
+END
+
+-- Executing the Stored Procedure
+EXEC TestMultiInput '111:2222, 123:677, 890:900';
