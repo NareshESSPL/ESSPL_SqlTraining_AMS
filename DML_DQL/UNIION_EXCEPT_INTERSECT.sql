8b78453fa7c6dd80d75f8e40f9287367e575eb98
Naresh Chandra pradhan <ncpbng1986@gmail.com>
2025-04-15 10:12:34 +0530

    Set Ops

---
+++ DML_DQL/UNIION_EXCEPT_INTERSECT.sql
@@ -0,0 +1,52 @@
+Create table Customer
+(
+  CustomerID BIGINT IDENTITY(1,1) NOT NULL Primary key,
+  CustomerName VARCHAR(100) NOT NULL,
+  PINCODE INT,
+  City VARCHAR(200) NULL
+)
+go
+
+Create table Supplier
+(
+  SupplierID BIGINT IDENTITY(1,1) NOT NULL Primary key,
+  SupplierName VARCHAR(100) NOT NULL,
+  PINCODE INT,
+  City VARCHAR(200) NULL
+)
+go
+
+insert into Customer values('Naresh', 111, 'BBSR')
+insert into Customer values('Suresh', 111, 'BBSR')
+insert into Customer values('Paresh', 222, 'CTC')
+insert into Customer values('Mahesh', 111,'JPR')
+insert into Customer values('Mahesh', 000,NULL)
+
+insert into Supplier values('Panda and Co', 111, 'BBSR')
+insert into Supplier values('Das and Co', 123, 'BBSR')
+insert into Supplier values('Nayak and Co', 777, 'CTC')
+insert into Supplier values('Aditya Groups', 111,'KJR')
+
+--get list of city we have both customer and suppier
+SELECT PINCODE, City FROM Customer
+UNION
+SELECT PINCODE, City FROM Supplier
+ORDER BY City;
+
+SELECT PINCODE, City FROM Customer
+UNION ALL
+SELECT PINCODE, City FROM Supplier
+ORDER BY City;
+
+--Only present in fist table
+SELECT PINCODE, City FROM Customer
+EXCEPT
+SELECT PINCODE, City FROM Supplier
+ORDER BY City;
+
+
+--Must present in both table
+SELECT PINCODE, City  FROM Customer
+INTERSECT
+SELECT PINCODE, City FROM Supplier
+ORDER BY City;
