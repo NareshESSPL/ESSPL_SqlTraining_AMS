a3b93d30c2da7fc61a5115618d6c74734fda444c
Naresh Chandra pradhan <ncpbng1986@gmail.com>
2025-04-16 10:33:31 +0530

    tran

---
+++ DML/Trsansaction WIth Error handling.sql
@@ -0,0 +1,30 @@
+BEGIN TRY
+
+ BEGIN TRAN
+  insert into Itemtype(ItemtypeId, ItemtypeName) values (111, 'testtype')
+  insert into Item(ItemId, ItemtypeId, Itemname) values (199, 879, 'testtype')
+ 
+ COMMIT TRAN
+
+END TRY
+BEGIN CATCH
+  
+  ROLLBACK
+  print 'rolledback'
+
+  DECLARE @Message NVARCHAR(MAX)
+  SELECT @Message = 
+        'ERROR_NUMBER : ' + CAST(ERROR_NUMBER() AS NVARCHAR(200)) + 
+        'ERROR_MESSAGE : ' + ERROR_MESSAGE() +
+        'ERROR_SEVERITY : ' + CAST(ERROR_SEVERITY() AS NVARCHAR(200)) +
+        'ERROR_STATE : ' + CAST(ERROR_STATE() AS NVARCHAR(200)) +
+        'ERROR_LINE : ' + CAST(ERROR_LINE() AS NVARCHAR(200)) 
+		
+  INSERT INTO ErrorLog VALUES('Query', @Message, GETUTCDATE())
+
+  PRINT 'Exception happened.Please refer the error log table'
+
+END CATCH
+
+select * from Item
+select * from ItemType
