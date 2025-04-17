f4cb103c382749b531024ccd4bdc8fef246c9d30
Naresh Chandra pradhan <ncpbng1986@gmail.com>
2025-04-17 09:38:14 +0530

    practice sample

---
+++ Practice/Sample_Table.sql
@@ -0,0 +1,84 @@
+--Create Database Practice
+--go
+
+--use Practice
+--go
+
+--CREATE TABLE Orders (
+--    Order_ID INT PRIMARY KEY,
+--    Customer_Name NVARCHAR(50),
+--    Product NVARCHAR(50),
+--    Quantity INT,
+--    Order_Date DATE,
+--    Total_Amount DECIMAL(10, 2),
+--    CATEGORY NVARCHAR(50)
+--);
+
+
+--INSERT INTO Orders (Order_ID, Customer_Name, Product, Quantity, Order_Date, Total_Amount, CATEGORY)
+--VALUES (101, 'Ananya', 'Laptop', 1, '2023-06-15', 55000.00, 'Electronics');
+
+--INSERT INTO Orders (Order_ID, Customer_Name, Product, Quantity, Order_Date, Total_Amount, CATEGORY)
+--VALUES (102, 'Bharat', 'Mobile Phone', 2, '2023-06-18', 60000.00, 'Electronics');
+
+--INSERT INTO Orders (Order_ID, Customer_Name, Product, Quantity, Order_Date, Total_Amount, CATEGORY)
+--VALUES (103, 'Chitra', 'Office Chair', 4, '2023-07-01', 28000.00, 'Furniture');
+
+--INSERT INTO Orders (Order_ID, Customer_Name, Product, Quantity, Order_Date, Total_Amount, CATEGORY)
+--VALUES (104, 'Dev', 'Coffee Maker', 1, '2023-07-04', 4000.00, 'Furniture');
+
+--INSERT INTO Orders (Order_ID, Customer_Name, Product, Quantity, Order_Date, Total_Amount, CATEGORY)
+--VALUES (105, 'Esha', 'Dining Table', 1, '2023-07-10', 15000.00, 'Furniture');
+
+--INSERT INTO Orders (Order_ID, Customer_Name, Product, Quantity, Order_Date, Total_Amount, CATEGORY)
+--VALUES (106, 'Ananya', 'Headphones', 1, '2023-06-16', 2000.00, 'Electronics');
+
+--INSERT INTO Orders (Order_ID, Customer_Name, Product, Quantity, Order_Date, Total_Amount, CATEGORY)
+--VALUES (107, 'Bharat', 'Charger', 1, '2023-06-20', 500.00, 'Electronics');
+
+--INSERT INTO Orders (Order_ID, Customer_Name, Product, Quantity, Order_Date, Total_Amount, CATEGORY)
+--VALUES (108, 'Harsh', 'Headphones', 1, '2023-07-16', 2000.00, 'Electronics');
+
+--INSERT INTO Orders (Order_ID, Customer_Name, Product, Quantity, Order_Date, Total_Amount, CATEGORY)
+--VALUES (109, 'Ishita', 'Headphones', 2, '2023-07-17', 4000.00, 'Electronics');
+
+--INSERT INTO Orders (Order_ID, Customer_Name, Product, Quantity, Order_Date, Total_Amount, CATEGORY)
+--VALUES (110, 'Jai', 'Headphones', 2, '2023-07-18', 4000.00, 'Electronics');
+
+--INSERT INTO Orders (Order_ID, Customer_Name, Product, Quantity, Order_Date, Total_Amount, CATEGORY)
+--VALUES (111, 'Kavita', 'Headphones', 6, '2023-07-19', 12000.00, 'Electronics');
+
+--Create table Discounts
+--(
+--  Discount_ID INT PRIMARY KEY,
+--  Product NVARCHAR(50),
+--  Discount_Percentage float
+--)
+
+--INSERT INTO Discounts (Discount_ID, Product, Discount_Percentage) VALUES (1, 'Headphones', 10)
+--INSERT INTO Discounts (Discount_ID, Product, Discount_Percentage) VALUES (2, 'Coffee Maker', 10)
+--INSERT INTO Discounts (Discount_ID, Product, Discount_Percentage) VALUES (3, 'Charger', 10)
+--INSERT INTO Discounts (Discount_ID, Product, Discount_Percentage) VALUES (4, 'Mobile Phone', 10)
+
+--CREATE TABLE Employees (
+--    EmployeeID INT PRIMARY KEY,
+--    FirstName NVARCHAR(50),
+--    LastName NVARCHAR(50),
+--    Position NVARCHAR(50),
+--    Salary DECIMAL(10, 2)
+--);
+
+--INSERT INTO Employees (EmployeeID, FirstName, LastName, Position, Salary)
+--VALUES (1, 'John', 'Doe', 'Manager', 75000.00);
+
+--INSERT INTO Employees (EmployeeID, FirstName, LastName, Position, Salary)
+--VALUES (2, 'Jane', 'Smith', 'Developer', 60000.00);
+
+--INSERT INTO Employees (EmployeeID, FirstName, LastName, Position, Salary)
+--VALUES (3, 'Alice', 'Johnson', 'Designer', 55000.00);
+
+--INSERT INTO Employees (EmployeeID, FirstName, LastName, Position, Salary)
+--VALUES (4, 'Bob', 'Brown', 'Analyst', 50000.00);
+
+
+
